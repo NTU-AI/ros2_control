6ff955e11a82d4e9a78ff5547e7f64af4a8bad17
GilmarCorreia <gilmarcorreiajeronimo@gmail.com>
2022-04-26 10:46:35 -0300

    alterando o array_data para vector

--- hardware_interface/include/hardware_interface/loaned_state_interface.hpp
+++ hardware_interface/include/hardware_interface/loaned_state_interface.hpp
@@ -58,7 +58,7 @@ public:
 
   double get_value() const { return state_interface_.get_value(); }
 
-  std::array<double,25> get_array_value() const { return state_interface_.get_array_value(); }
+  std::vector<double> get_array_value() const { return state_interface_.get_array_value(); }
 
 protected:
   StateInterface & state_interface_;
